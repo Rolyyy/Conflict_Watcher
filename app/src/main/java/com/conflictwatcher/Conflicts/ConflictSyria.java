a0f3b137c167a09cadcaa8e2020153b28f9acd83
Roland Butacu <1541224@brunel.ac.uk>
2020-02-13 17:23:02 +0000

    Added back button in SyriaConflictActivity. Added Text in Conflicts Activity.

--- app/src/main/java/com/conflictwatcher/Conflicts/ConflictSyria.java
+++ app/src/main/java/com/conflictwatcher/Conflicts/ConflictSyria.java
@@ -1,9 +1,9 @@
 package com.conflictwatcher.Conflicts;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import com.conflictwatcher.R;
 
 public class ConflictSyria extends AppCompatActivity {
@@ -12,5 +12,14 @@ public class ConflictSyria extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conflict_syria);
+
+        Button backbutton = findViewById(R.id.syria_button_back);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }

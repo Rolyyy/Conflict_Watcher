8553c7cb1888c311d30cbbb08bcfd81890a3ed12
Roland Butacu <1541224@brunel.ac.uk>
2020-03-07 20:09:07 +0000

    Added conflict information for Somalia and Yemen.

--- app/src/main/java/com/conflictwatcher/Conflicts/ConflictYemen.java
+++ app/src/main/java/com/conflictwatcher/Conflicts/ConflictYemen.java
@@ -3,6 +3,8 @@ package com.conflictwatcher.Conflicts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.conflictwatcher.R;
 
@@ -12,5 +14,13 @@ public class ConflictYemen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conflict_yemen);
+
+        Button backbutton = findViewById(R.id.yemen_button_back);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
